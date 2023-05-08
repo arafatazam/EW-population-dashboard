2d662a7e0005d45f5b8ed627462155ffd36d4620
Arafat Azam <arafatazam17@gmail.com>
2023-05-08 22:46:23 +0100

    Bar chart by age

--- solution.py
+++ solution.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
 INPUT_FILE = "EW_Religion_Sex_Age_Ethnic_group.csv"
 OUTPUT_FILE = "21087019.png"
@@ -15,8 +13,8 @@ def read_data():
 
 
 def draw_heading(ax: plt.Axes):
-    ax.text(0.5, 0.5, "England and Walse Census 2021: Religion, ethnicity and age",
-            fontsize=12, ha="center")
+    ax.text(0.5, 0.5, "UK Census 2021: Religion, Ethnicity and Age of England & Wales",
+            fontsize=30, ha="center", va="center")
     for spine in ax.spines.values():
         spine.set_visible(False)
 
@@ -24,11 +22,31 @@ def draw_heading(ax: plt.Axes):
                    right=False, labelleft=False, labelbottom=False)
 
 
+def draw_age_barchart(ax: plt.Axes):
+    df = DATA.groupby('Age Code', as_index=False)['Observation'].sum()
+    sns.barplot(x='Age Code', y='Observation', data=df, ax=ax)
+
+    # Set labels for x and y axes
+    ax.set_xlabel('Age', fontsize=20)
+    ax.set_ylabel('Observation', fontsize=20)
+
+    # Remove top and right spines of the plot
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    ax.set_xticks(range(0, 91, 5))
+    ax.set_xticklabels(df['Age Code'][::5])
+
+    # Remove tick marks from x and y axes
+    ax.tick_params(axis='both', which='both', length=0, labelsize=15)
+
+
 def main():
     read_data()
-    fig = plt.figure()
+    fig = plt.figure(figsize=(20, 12))
     gs = fig.add_gridspec(20, 12)
     draw_heading(plt.subplot(gs[:2, :]))
+    draw_age_barchart(plt.subplot(gs[2:8, :]))
     plt.show()
 
 
