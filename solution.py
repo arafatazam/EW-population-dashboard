52e62e10b625a6bad2b2846dc3b4f6e8886c7e02
Arafat Azam <arafatazam17@gmail.com>
2023-05-10 22:11:34 +0100

    Descriptions added

--- solution.py
+++ solution.py
@@ -43,22 +43,24 @@ def draw_age_barchart(ax: plt.Axes):
 def draw_religion_piechart(ax: plt.Axes):
     df = DATA.groupby('Religion', as_index=False)['Observation'].sum(
     ).sort_values(by='Observation', ascending=False)
+    df = df[df['Observation'] > 0]
     colors = sns.color_palette('muted')[0:10]
     ax.pie(df['Observation'], colors=colors,
            autopct=lambda pct: f'{pct:.1f}%' if pct > 3 else '', startangle=210)
     ax.axis('equal')
-    ax.legend(df['Religion'], loc='center left')
+    ax.legend(df['Religion'], loc='center left', fontsize=14)
     ax.set_title('Distribution by Religion', fontsize=20)
 
 
 def draw_ethnicity_piechart(ax: plt.Axes):
     df = DATA.groupby('Ethnic Group', as_index=False)[
         'Observation'].sum().sort_values(by='Observation', ascending=False)
+    df = df[df['Observation'] > 0]
     colors = sns.color_palette('muted')[4:]
     ax.pie(df['Observation'], colors=colors,
            autopct=lambda pct: f'{pct:.1f}%' if pct > 3 else '', startangle=40)
     ax.axis('equal')
-    ax.legend(df['Ethnic Group'], loc='center right')
+    ax.legend(df['Ethnic Group'], loc='center right', fontsize=14)
     ax.set_title('Distribution by Ethnic Group', fontsize=20)
 
 
@@ -80,22 +82,28 @@ def draw_description_box(txt: str, ax: plt.Axes, ha: str = 'left'):
 
 def main():
     read_data()
+    plt.rcParams['font.family'] = ['DejaVu Sans', 'Helvetica', 'Arial']
     fig = plt.figure(figsize=(FIG_WIDTH, FIG_LENGTH), dpi=300)
     gs = fig.add_gridspec(
         nrows=FIG_LENGTH, ncols=FIG_WIDTH, wspace=2, hspace=1)
     draw_heading(plt.subplot(gs[:2, :]))
 
-    lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed magna vel est euismod auctor.'
-
-    draw_description_box(lorem, plt.subplot(gs[2, :]))
+    description = 'England and wales age distribution barplot is narrower in the beginning indicating a falling birth-rate. ' +\
+        'Then there are higher bars in the working age portion 16-64 which indicates an aging population and the effect of ' +\
+        'net migration'
+    draw_description_box(description, plt.subplot(gs[2, :]))
     draw_age_barchart(plt.subplot(gs[3:8, :]))
 
+    description = 'Christianity(46.2%) is the largest religion in England & Wales, ' +\
+        'followed by no religion(37.2%). Islam(6.5%) is the second largest among those who follows religion.'
     draw_religion_piechart(plt.subplot(gs[9:14, :10]))
-    draw_description_box(lorem, plt.subplot(gs[9:14, 10:]))
+    draw_description_box(description, plt.subplot(gs[9:14, 10:]))
 
-    draw_description_box(lorem, plt.subplot(gs[15:20, :6]))
+    description = 'England & Wales is a ethnically diverse place. White ethnicities(81.7%) are the largest ethnic group,' +\
+        'followed by Asian ethnicities(9.3%) and Black ethnicities(4%)'
+    draw_description_box(description, plt.subplot(gs[15:20, :6]))
     draw_ethnicity_piechart(plt.subplot(gs[15:20, 6:]))
-    fig.savefig(OUTPUT_FILE, dpi=300)
+    fig.savefig(OUTPUT_FILE)
 
 
 if __name__ == "__main__":
