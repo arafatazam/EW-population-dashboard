e64e6b9d63d8643ece7767b9d64a2fdef67e32e1
Arafat Azam <arafatazam17@gmail.com>
2023-05-10 06:27:09 +0100

    Religion and Ethnicity Picharts

--- solution.py
+++ solution.py
@@ -13,11 +13,8 @@ def read_data():
 
 
 def draw_heading(ax: plt.Axes):
-    ax.text(0.5, 0.5, "UK Census 2021: Religion, Ethnicity and Age of England & Wales",
+    ax.text(0.5, 0.5, "England & Wales Census 2021: Religion, Ethnicity and Age",
             fontsize=30, ha="center", va="center")
-    for spine in ax.spines.values():
-        spine.set_visible(False)
-
     ax.tick_params(top=False, bottom=False, left=False,
                    right=False, labelleft=False, labelbottom=False)
 
@@ -41,13 +38,38 @@ def draw_age_barchart(ax: plt.Axes):
     ax.tick_params(axis='both', which='both', length=0, labelsize=15)
 
 
+def draw_religion_piechart(ax: plt.Axes):
+    df = DATA.groupby('Religion', as_index=False)['Observation'].sum(
+    ).sort_values(by='Observation', ascending=False)
+    colors = sns.color_palette('muted')[0:10]
+    ax.pie(df['Observation'], colors=colors,
+           autopct=lambda pct: f'{pct:.1f}%' if pct > 3 else '', startangle=210)
+    ax.axis('equal')
+    ax.legend(df['Religion'], loc='center left')
+    ax.set_title('Distribution by Religion', fontsize=20)
+
+
+def draw_ethnicity_piechart(ax: plt.Axes):
+    df = DATA.groupby('Ethnic Group', as_index=False)[
+        'Observation'].sum().sort_values(by='Observation', ascending=False)
+    colors = sns.color_palette('muted')[4:]
+    ax.pie(df['Observation'], colors=colors,
+           autopct=lambda pct: f'{pct:.1f}%' if pct > 3 else '', startangle=40)
+    ax.axis('equal')
+    ax.legend(df['Ethnic Group'], loc='center right')
+    ax.set_title('Distribution by Ethnic Group', fontsize=20)
+
+
 def main():
     read_data()
-    fig = plt.figure(figsize=(20, 12))
-    gs = fig.add_gridspec(20, 12)
+    fig = plt.figure(figsize=(16, 25))
+    fig.set_facecolor('lightblue')
+    gs = fig.add_gridspec(nrows=25, ncols=16, wspace=2, hspace=1)
     draw_heading(plt.subplot(gs[:2, :]))
-    draw_age_barchart(plt.subplot(gs[2:8, :]))
-    plt.show()
+    draw_age_barchart(plt.subplot(gs[2:5, :]))
+    draw_religion_piechart(plt.subplot(gs[6:10, :8]))
+    draw_ethnicity_piechart(plt.subplot(gs[6:10, 8:]))
+    fig.savefig(OUTPUT_FILE, dpi=300)
 
 
 if __name__ == "__main__":
